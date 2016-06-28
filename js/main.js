f504b7063e3a8eca89b000ebd95535732d119e13
Alice <alice.wu.nz@gmail.com>
2016-06-28 21:50:35 +1200

    Using jquery to set up logo, back-bird, fore-bird transform css.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(window).scroll(function(){
    var scroll = $(this).scrollTop();
-   console.log(scroll);
+   
+   $('.logo').css({
+   	'transform' : 'rotate(7deg) translate(0px, '+ scroll/2 +'%)'
+   });
+   $('.back-bird').css({
+   	'transform' : 'translate(0px, '+ scroll/4 +'%)'
+   });
+   $('.fore-bird').css({
+   	'transform' : 'translate(0px, -'+ scroll/50 +'%)'
+   });
 });
